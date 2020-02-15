10db1c190337422af619f4a59fb99b5b9f1c9f69
Jeff Fox <luck7@live.cn>
2020-02-15 17:35:43 +0800

    Add a model

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -4,39 +4,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using RazorPagesMovie.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie
 {
     public class Startup
+    {    public Startup(IConfiguration configuration, IWebHostEnvironment env)
     {
-        public Startup(IConfiguration configuration)
+        Environment = env;
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Environment { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        if (Environment.IsDevelopment())
         {
-            Configuration = configuration;
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+            options.UseSqlite(
+                Configuration.GetConnectionString("MovieContext")));
+        }
+        else
+        {
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("MovieContext")));
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
+        services.AddRazorPages();
+    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -51,9 +58,15 @@ namespace RazorPagesMovie
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
         }
     }
 }
